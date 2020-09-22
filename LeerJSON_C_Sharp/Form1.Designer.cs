92294976b4ad2c2f5e1d37533d626cf7c183759a
pepoflex <jrclurita@gmail.com>
2020-09-21 21:44:48 -0500

    Cambios

---
+++ LeerJSON_C_Sharp/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace LeerJSON_C_Sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(286, 108);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Leer JSON en ComboBox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ LeerJSON_C_Sharp/Form1.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeerJSON_C_Sharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Leemos el archivo 'postres.json' 
+            using (StreamReader archivo = File.OpenText(@"C:/Users/carlos/source/repos/LeerJSON_C_Sharp/LeerJSON_C_Sharp/postres.json"))
+
+            {
+                // Leemos los datos del archivo 'postre.json' desde el inicio hasta el final 
+                string json = archivo.ReadToEnd();
+
+                // Deserializamos el objeto 'json' 
+                dynamic miarray = JsonConvert.DeserializeObject(json);
+
+                // Recorremos el array 'miarray' 
+                foreach (var item in miarray)
+                {
+                    // Agregamos el campo nombre del archivo 'postres.json' al ComboBox llamado 'comboBox1' 
+                    comboBox1.Items.Add(item.nombre);
+                    //Console.WriteLine("Datos: " + item.nombre);
+
+                }
+
+            }
+        }
+    }
+}
